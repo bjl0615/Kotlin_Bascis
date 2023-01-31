9bd2b0df1575f72a7c0fd9e1bd2d8b95dca6a5be
bjl0615 <bjl0615@gmail.com>
2023-01-31 16:37:11 +0900

    Integer_Types

---
+++ app/src/main/java/eu/tutorials/kotlinbasics/Number.kt
@@ -0,0 +1,25 @@
+package eu.tutorials.kotlinbasics
+
+fun main() {
+    /*
+        This
+        is a multiple
+        comment
+     */
+
+    // type string
+    val myName = "Lee"
+    // type int
+    // Integer Types : Byte(8 bit), Short (16 bit),
+    // Int (32bit), Long(64 bit)
+    var myAge = 25
+
+    val myByte : Byte = 13
+    val myShort : Short = 125
+    val myInt : Int = 123123123
+    val myLong : Long = 39_812_309_487_120_394
+
+    //Floating Point number Types : Float(32 bit), Double(64 bit)
+    val myFloat : Float = 13.37F
+    val myDouble : Double = 3.14159265358979323846
+}
