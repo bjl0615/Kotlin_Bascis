0ced74c14085f662bc6f7fe6c7c3b39a550c73ca
bjl0615 <bjl0615@gmail.com>
2023-02-01 14:20:00 +0900

    Scope

---
+++ app/src/main/java/eu/tutorials/kotlinbasics/Scope.kt
@@ -0,0 +1,12 @@
+package eu.tutorials.kotlinbasics
+
+fun main() {
+    myFunction(5)
+}
+
+// this a is a parameter
+fun myFunction( a : Int ) {
+    // a is a variable
+    var b = a
+    println("b is $b")
+}
